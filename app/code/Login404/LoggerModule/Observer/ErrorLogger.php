bd2d774f06067667f47daa7f9ab062d4461c26d6
Akshayan <akshay.vm@eglobeits.com>
2023-11-09 11:29:19 +0530

    refactor: add space in the logging url

--- app/code/Login404/LoggerModule/Observer/ErrorLogger.php
+++ app/code/Login404/LoggerModule/Observer/ErrorLogger.php
@@ -11,30 +11,31 @@ use Magento\Framework\App\Request\Http;
 class ErrorLogger implements ObserverInterface
 {
     protected $logger;
-    //protected $counter;
+    protected $counter;
 
     public function __construct(\Psr\Log\LoggerInterface $logger)
     {
         $this->logger = $logger;
-        // $this->counter = 0;
+        $this->counter = 0;
     }
 
     public function execute(Observer $observer)
     {
         $response = $observer->getEvent()->getResponse();
         if ($response && $response->getHttpResponseCode() == 404) {
+            $this->counter++;
             // $this->logger->error('404 Page Not Found: ' . $this->getCurrentUrl());
             $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/custom.log');
             $logger = new \Zend_Log();
             $logger->addWriter($writer);
-            $logger->info('404 Page Not Foundd: ' . $this->getCurrentUrl());
-            // $this->counter++;
+            $logger->info('404 Page Not Foundd: ' . $this->getCurrentUrl() . " " . 'Count: ' . $this->counter);
         }
 
         //  $logger->info('404 Page Not Foundd: ' . print_r($observer->getEvent()->debug(), true));
     }
     protected function getCurrentUrl()
     {
+        // returns the current URL if its set
         return isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
     }
 }
