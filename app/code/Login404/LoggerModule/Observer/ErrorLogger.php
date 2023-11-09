8019c0dd2850c99558aed88f47e6bdd594610f04
Akshayan <akshay.vm@eglobeits.com>
2023-11-09 11:20:24 +0530

    refactor: add 404 page url log into custom log file

--- app/code/Login404/LoggerModule/Observer/ErrorLogger.php
+++ app/code/Login404/LoggerModule/Observer/ErrorLogger.php
@@ -21,18 +21,20 @@ class ErrorLogger implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        // $response = $observer->getEvent()->getResponse();
-        // if ($response && $response->getHttpResponseCode() == 404) {
-        //     $this->logger->error('404 Page Not Found: ' . $this->getCurrentUrl());
-        //     $this->counter++;
-        // }
-        $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/custom.log');
-        $logger = new \Zend_Log();
-        $logger->addWriter($writer);
-        $logger->info('Observer executed' . print_r($observer->getEvent()->debug(), true));
+        $response = $observer->getEvent()->getResponse();
+        if ($response && $response->getHttpResponseCode() == 404) {
+            // $this->logger->error('404 Page Not Found: ' . $this->getCurrentUrl());
+            $writer = new \Zend_Log_Writer_Stream(BP . '/var/log/custom.log');
+            $logger = new \Zend_Log();
+            $logger->addWriter($writer);
+            $logger->info('404 Page Not Foundd: ' . $this->getCurrentUrl());
+            // $this->counter++;
+        }
+
+        //  $logger->info('404 Page Not Foundd: ' . print_r($observer->getEvent()->debug(), true));
     }
-    // protected function getCurrentUrl()
-    // {
-    //     return isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-    // }
+    protected function getCurrentUrl()
+    {
+        return isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+    }
 }
