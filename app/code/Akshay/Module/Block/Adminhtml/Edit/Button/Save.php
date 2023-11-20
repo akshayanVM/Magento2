fbe01a76dd347aca6da9d77aa2b83a06562967f6
Akshayan <akshay.vm@eglobeits.com>
2023-11-20 12:27:30 +0530

    refactor:  add save button generic codes

---
+++ app/code/Akshay/Module/Block/Adminhtml/Edit/Button/Save.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Akshay\Module\Block\Adminhtml\Edit\Button;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Magento\Ui\Component\Control\Container;
+
+class Save extends Generic implements ButtonProviderInterface
+{
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'customblog_new_form.customblog_new_form',
+                                'actionName' => 'save',
+                                'params' => [
+                                    false,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
