d3defe4e930739ed26c38efca035704faf787aa0
Akshayan <akshay.vm@eglobeits.com>
2023-11-22 10:20:24 +0530

    refactor: add details to database

--- app/code/Akshay/Module/Controller/Adminhtml/Index/Save.php
+++ app/code/Akshay/Module/Controller/Adminhtml/Index/Save.php
@@ -25,22 +25,29 @@ class Save extends Action
 
     public function execute()
     {
-        try {
-            $data = (array)$this->getRequest()->getParams();
-            $name = $data['name'];
-            $email = $data['email'];
-            print_r($data);
-            if ($name && $email) {
-                $model = $this->collectionFactory->create();
-                // print_r($model);
-                $model->setData($data)->save();
-                $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
-            }
-        } catch (\Exception $e) {
-            $this->messageManager->addErrorMessage($e, __("We can\'t submit your request, Please try again."));
-        }
+        // try {
+        $data = (array)$this->getRequest()->getParams();
+        // var_dump($data);
+        $test = $data['customer_name'];
+        $test1 = $data['email'];
+        // var_dump($test);
+        // dd();
+        // dd();
+        // print_r($data);
+        // if ($data) {
+        $model = $this->collectionFactory->create();
+        // print_r($model);
+        // dd();
+        $model->setData(['name' => $test, 'email' => $test1])->save();
+        // $model->setData()->save();
+        $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
+        // }
+        // } catch (\Exception $e) {
+        //     $this->messageManager->addErrorMessage($e, __("We can\'t submit your request, Please try again."));
+        // }
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+        $resultRedirect->setPath('custom_module/Index');
+        // $resultRedirect->setUrl($this->_redirect->getRefererUrl());
         return $resultRedirect;
     }
 }
