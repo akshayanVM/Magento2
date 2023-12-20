dcdced2a20b7afa2e4dc1cf98841b18e0dba36c0
akshayanVM <akshay.vm@eglobeits.com>
2023-12-20 20:40:56 +0530

    refactor: add form page sample template

---
+++ app/code/Egits/Integration/Block/Adminhtml/Edit/Button/Add.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Egits\Integration\Block\Adminhtml\Edit\Button;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Magento\Ui\Component\Control\Container;
+
+class Add extends Generic implements ButtonProviderInterface
+{
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Add'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'brands_module_listing.brands_module_listing',
+                                'actionName' => 'save',
+                                'params' => [
+                                    false,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
