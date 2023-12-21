2779866f4ba1b74be3a9f4673fa16898f3efbd28
akshayanVM <akshay.vm@eglobeits.com>
2023-12-21 12:01:54 +0530

    refactor: add back and save button on the add new brands page

---
+++ app/code/Egits/Integration/Block/Adminhtml/Edit/Button/Save.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Egits\Integration\Block\Adminhtml\Edit\Button;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+use Magento\Ui\Component\Control\Container;
+
+class Save extends Generic implements ButtonProviderInterface
+{
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => [
+                    'buttonAdapter' => [
+                        'actions' => [
+                            [
+                                'targetName' => 'brands_module_form.brands_module_form',
+                                'actionName' => 'save',
+                                'params' => [
+                                    false,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
