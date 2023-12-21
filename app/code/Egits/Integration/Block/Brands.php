d5f86280d4b6e9f5ac23399cd9ad7ba16fcca1a4
akshayanVM <akshay.vm@eglobeits.com>
2023-12-21 14:15:49 +0530

    refactor: add top brands slider to the front end and populate the data

---
+++ app/code/Egits/Integration/Block/Brands.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Egits\Integration\Block;
+
+use Magento\Framework\View\Element\Template;
+use Egits\Integration\Model\ResourceModel\Post\CollectionFactory;
+
+class Brands extends Template
+{
+    protected $brandsCollection;
+    public function __construct(
+        Template\Context $context,
+        CollectionFactory $brandsCollection,
+        array $data = []
+    ) {
+        $this->brandsCollection = $brandsCollection;
+        parent::__construct($context, $data);
+    }
+    public function getDataForPHTML()
+    {
+        $brands = $this->brandsCollection->create();
+
+        // return "brands collection";
+        // $categories->addAttributeToSelect('*');
+        // $categories->addAttributeToFilter('enable_category', 1);
+        foreach ($brands as $brand) {
+
+            //            var_dump($category->getData());
+            //            dd();
+            $collection[] = $brand->getData();
+        }
+
+        return $collection;
+    }
+}
