9e0df88679e755bd79a3a344a67330c989f6858d
akshayanVM <akshay.vm@eglobeits.com>
2024-01-04 17:56:13 +0530

    refactor: add widgets and blocks using data patch

--- app/code/Egits/Integration/view/frontend/web/js/script.js
+++ app/code/Egits/Integration/view/frontend/web/js/script.js
@@ -4,15 +4,15 @@ require(["jquery"], function ($) {
             e.preventDefault();
             let productId = $(this).data("product-entity-id");
             let formKey = $(this).data("form-key");
-            var formKeyTest = $('input[name="form_key"]').val();
+            // var formKeyTest = $('input[name="form_key"]').val();
             console.log(productId);
-            console.log(formKeyTest);
+            // console.log(formKeyTest);
             $.ajax({
                 // url: "http://test.magento2.com/index.php/wishlist/index/add/", // Replace with your controller URL
                 url: "/wishlist/index/add/product/" + productId + "/",
                 type: "POST",
                 // dataType: "json",
-                data: { productId: productId, form_key: formKey }, // it wasnt working because the form_key name was the same
+                data: { productId: productId, form_key: formKey }, // it wasnt working because the data name was supposed to be 'form_key'
                 success: function (response) {
                     console.log(response);
                     window.location.href = "/wishlist/index/index";
