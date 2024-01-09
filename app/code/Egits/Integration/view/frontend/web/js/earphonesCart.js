3e4a89a75daf1f83c5141b7744185af772fc7107
akshayanVM <akshay.vm@eglobeits.com>
2024-01-09 16:34:06 +0530

    refactor: add earphones product add to cart functionality

---
+++ app/code/Egits/Integration/view/frontend/web/js/earphonesCart.js
@@ -0,0 +1,34 @@
+require(["jquery"], function ($) {
+    $(document).ready(function () {
+        $(" .action.tocart.primary").click(function (e) {
+            e.preventDefault();
+            let productId = $(this).data("earphone-cart-entity-id");
+            let formKey = $(this).data("earphone-cart-form-key");
+            // var formKeyTest = $('input[name="form_key"]').val();
+            console.log(productId);
+            // console.log(formKeyTest);
+            $.ajax({
+                // url: "http://test.magento2.com/index.php/wishlist/index/add/", // Replace with your controller URL
+                url: "/checkout/cart/add/product/" + productId + "/",
+                type: "POST",
+                // dataType: "json",
+                data: { productId: productId, form_key: formKey }, // it wasnt working because the data name was supposed to be 'form_key'
+                success: function (response) {
+                    console.log(response);
+                    window.location.href = "/checkout/cart";
+                    // if (response.success) {
+                    //     alert("Value sent successfully!");
+                    // } else {
+                    //     alert("Failed to send value.");
+                    // }
+                },
+                error: function (xhr, status, error) {
+                    console.error("AJAX Error:", status, error);
+                    // alert("Error occurred while sending value.");
+                },
+            });
+        });
+    });
+});
+
+//generate the form key
